3c62815d95ac3b6048fb367bf6266eb6379ab0b9
tranvuongduy2003 <tranvuongduy2003@gmail.com>
2023-03-20 22:45:46 +0700

    add order screen

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop_app/providers/orders.dart';
 
 import '../providers/cart.dart';
 import '../widgets/cart_item_widget.dart';
@@ -42,7 +43,13 @@ class CartScreen extends StatelessWidget {
                     backgroundColor: Theme.of(context).primaryColor,
                   ),
                   TextButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      Provider.of<Orders>(context, listen: false).addOrder(
+                        cart.items.values.toList(),
+                        cart.totalAmount,
+                      );
+                      cart.clear();
+                    },
                     style: ButtonStyle(
                         foregroundColor: MaterialStateProperty.all(
                             Theme.of(context).primaryColor)),
